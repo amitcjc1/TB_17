34bccf437d286ae554cef7ca81a07c6e5a6d8afb
amit <amitcjcpune@gmail.com>
2022-10-15 11:26:02 +0530

    by...xyz

--- Project1Git/src/Demo.java
+++ Project1Git/src/Demo.java
@@ -1,6 +1,13 @@
 
 public class Demo {
-public static void main(String[] args) {
+public static void main(String[] args) 
+{
 	System.out.println("Hello Demo");
 }
+
+public void m1()
+{
+	System.out.println("M1");
+}
+
 }
